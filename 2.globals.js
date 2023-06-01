24a148f2d6f7b163bbc878493a0843283a327239
GiMorales <113841874+GiMorales@users.noreply.github.com>
2023-06-01 11:01:25 -0700

    firts commit

---
+++ 2.globals.js
@@ -0,0 +1,13 @@
+//GLOBALS - NO WINDOW!!!
+
+//  __dirname     - path to current directory
+//  __filename    - file name
+//  requiere      - function to use modules (CommonJS)
+//  module        - info about current module (file)
+//  process        - info about env where the program
+
+console.log(process )
+setInterval(() => {
+  console.log('hello world')
+}, 1000)
+
---
+++ 4.names.js
@@ -0,0 +1,8 @@
+//local
+const secret = 'SUPER SECRET'
+//share
+const john ='jonh'
+const peter ='peter'
+
+
+ module.exports = { john, peter}
